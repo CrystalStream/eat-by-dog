67662e38980acf82b9b17b29e2b81d8aaa966ddd
Christofer Flores <christofer@michelada.io>
2018-11-05 14:04:41 -0600

    Modify the project to use scss instead of sass and modify the port

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
+  indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
